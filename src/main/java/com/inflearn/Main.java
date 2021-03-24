7fb8552eba763157614c3c24c3fdbe3544fc41c4
YunKyoung <06007sk@gmail.com>
2021-03-24 18:27:44 +0900

    Initial commit

---
+++ src/main/java/com/inflearn/Main.java
@@ -0,0 +1,14 @@
+package com.inflearn;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("test");
+        System.out.println("222sss2");
+        System.out.println("aaa");
+        System.out.println("aasssa");
+        System.out.println("aaa");
+    }
+}
